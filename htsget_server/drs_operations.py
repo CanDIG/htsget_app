2a30d8485cbce092b9fd43086acbf1d53460e26a
Daisie Huang <daisieh@Daisies-MacBook-Pro.local>
2021-10-01 22:57:12 -0700

    cleanup

--- htsget_server/drs_operations.py
+++ htsget_server/drs_operations.py
@@ -1,14 +1,11 @@
-import os
-from flask import send_file, jsonify
-from pysam import VariantFile, AlignmentFile
 from minio import Minio
-from minio.error import InvalidResponseError
 import connexion
 import database
+import configparser
+from pathlib import Path
 
 config = configparser.ConfigParser()
 config.read(Path('./config.ini'))
-DB_PATH = config['paths']['DBPath']
 MINIO_END_POINT = config['minio']['EndPoint']
 MINIO_ACCESS_KEY = config['minio']['AccessKey']
 MINIO_SECRET_KEY = config['minio']['SecretKey']
@@ -27,7 +24,7 @@ def get_access_url(object_id, access_id):
             access_key=MINIO_ACCESS_KEY,
             secret_key=MINIO_SECRET_KEY
         )
-        url = client.presigned_get_object(bucket_name="testdaisieh", object_name=access_id)
+        url = client.presigned_get_object(bucket_name="testdaisieh", object_name=object_id)
     except Exception as e:
         return {"message": str(e)}, 500
     return {"url": url}, 200
