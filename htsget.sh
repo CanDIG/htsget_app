2013fc325aeb15f89185afac6aae5df148fb6900
Jagdeep Sason <jagdeep.sason@uhn.ca>
2021-11-19 10:58:41 -0500

    Pushing bash script

---
+++ htsget.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Check if command exists
+if ! command -v python3 &>/dev/null; then
+     echo "python3: command not found"
+     exit 127
+fi
+
+# Check python version
+if ! [[ $(python3 --version | awk '{print $2}') =~ 3.6.* ]]; then 
+     echo "Need python 3.6 to run the script"
+     exit 1
+fi
+
+#Installing libraries 
+python3 setup.py install
+python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements_dev.txt
+
+# Running Scripts
+python3 htsget_server/server.py &
+sleep 5
+pytest tests/test_htsget_server.py
+
+
