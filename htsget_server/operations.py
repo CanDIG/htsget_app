bc132ad1163acc079a3cf4e08943db30989e2913
Daisie Huang <daisieh@gmail.com>
2022-05-02 23:34:41 -0700

    use url_root to pick up scheme

--- htsget_server/operations.py
+++ htsget_server/operations.py
@@ -98,7 +98,7 @@ def _create_slice(arr, id, reference_name, slice_start, slice_end, file_type):
     :param slice_start: Starting index of a slice
     :param slice_end: Ending index of a slice
     """
-    url = f"http://{request.host}/htsget/v1/{file_type}s/data/{id}?referenceName={reference_name}&start={slice_start}&end={slice_end}"
+    url = f"{request.url_root}/htsget/v1/{file_type}s/data/{id}?referenceName={reference_name}&start={slice_start}&end={slice_end}"
     arr.append({'url': url, })
 
 
@@ -124,7 +124,7 @@ def _create_slices(chunk_size, id, reference_name, start, end, file_type):
             slice_start = slice_end
         _create_slice(urls, id, reference_name, slice_start, end, file_type)
     else:  # One slice only
-        url = f"http://{request.host}/htsget/v1/{file_type}s/data/{id}"
+        url = f"{request.url_root}/htsget/v1/{file_type}s/data/{id}"
         if reference_name and start and end:
             url += f"?referenceName={reference_name}&start={start}&end={end}"
         urls.append({"url": url})
@@ -229,7 +229,7 @@ def _get_urls(file_type, id, reference_name=None, start=None, end=None, _class=N
     gen_obj = _get_genomic_obj(id)
     if gen_obj is not None:
         if _class == "header":
-            urls = [{"url": f"http://{request.host}/htsget/v1/{file_type}s/data/{id}?class=header",
+            urls = [{"url": f"{request.url_root}/htsget/v1/{file_type}s/data/{id}?class=header",
             "class": "header"}]
         else:
                 file_in = gen_obj["file"]
