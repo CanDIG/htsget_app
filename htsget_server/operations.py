1551dc183139252f4bc3fd7fce230f503c74f31e
Daisie Huang <daisieh@gmail.com>
2022-10-13 22:39:44 -0700

    refactor

--- htsget_server/operations.py
+++ htsget_server/operations.py
@@ -368,34 +368,13 @@ def _get_genomic_obj(object_id):
     index_file = None
     variant_file = None
     read_file = None
-    file_format = None
     result = None
 
     with tempfile.TemporaryDirectory() as tempdir:
-        (drs_obj, status_code) = drs_operations.get_object(object_id)
-        if status_code != 200:
-            return None
-
-        # drs_obj should have two contents objects
-        for contents in drs_obj["contents"]:
-            # get each drs object (should be the genomic file and its index)
-            print(contents)
-            (sub_obj, status_code) = drs_operations.get_object(contents["name"])
-
-            # if sub_obj.name matches an index file regex, it's an index file
-            index_match = re.fullmatch('.+\.(..i)$', sub_obj["name"])
-
-            # if sub_obj.name matches a bam/sam/cram file regex, it's a read file
-            read_match = re.fullmatch('.+\.(.+?am)$', sub_obj["name"])
-
-            # if sub_obj.name matches a vcf/bcf file regex, it's a variant file
-            variant_match = re.fullmatch('.+\.(.cf)(\.gz)*$', sub_obj["name"])
-
-            if read_match is not None:
-                file_format = read_match.group(1).upper()
-            elif variant_match is not None:
-                file_format = variant_match.group(1).upper()
-
+        drs_obj = _describe_drs_object(object_id)
+        for content_type in ['main', 'index']:
+            sub_obj_id = drs_obj[content_type]
+            (sub_obj, status_code) = drs_operations.get_object(sub_obj_id)
             # get access_methods for this sub_obj
             for method in sub_obj["access_methods"]:
                 if "access_id" in method and method["access_id"] != "":
@@ -407,12 +386,13 @@ def _get_genomic_obj(object_id):
                             with requests.get(url["url"], stream=True) as r:
                                 with r.raw as content:
                                     f.write(content.data)
-                        if index_match is not None:
+                        if content_type == 'index':
                             index_file = f_path
-                        elif read_match is not None:
-                            read_file = f_path
-                        elif variant_match is not None:
-                            variant_file = f_path
+                        elif content_type == 'main':
+                            if drs_obj['type'] == 'read':
+                                read_file = f_path
+                            else:
+                                variant_file = f_path
                     else:
                         return {"error": url, "status_code": status_code}
                 else:
@@ -420,18 +400,19 @@ def _get_genomic_obj(object_id):
                     url_pieces = urlparse(method["access_url"]["url"])
                     if url_pieces.scheme == "file":
                         if url_pieces.netloc == "" or url_pieces.netloc == "localhost":
-                            if index_match is not None:
+                            if content_type == 'index':
                                 index_file = url_pieces.path
-                            elif read_match is not None:
-                                read_file = url_pieces.path
-                            elif variant_match is not None:
-                                variant_file = url_pieces.path
+                            elif content_type == 'main':
+                                if drs_obj['type'] == 'read':
+                                    read_file = url_pieces.path
+                                else:
+                                    variant_file = url_pieces.path
         if variant_file is not None:
             result = VariantFile(variant_file, index_filename=index_file)
         elif read_file is not None:
             result = AlignmentFile(read_file, index_filename=index_file)
 
-    return { "file": result, "file_format": file_format }
+    return { "file": result, "file_format": drs_obj['format'] }
 
 
 # describe an htsget DRS object, but don't open it
