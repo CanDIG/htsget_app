4f3301e0ce77b83a15d4fef3c399fc35c7b83752
Daisie Huang <daisieh@gmail.com>
2023-01-16 21:36:03 -0800

    separate out create_db.sh

---
+++ create_db.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -Euo pipefail
+
+db=${DB_PATH:-data/files.db}
+
+echo "database is at $db"
+pwd
+
+# initialize the db if it's not already there:
+sqlite3 -bail $db "SELECT * from ncbiRefSeq limit 1"
+if [[ $? -eq 1 ]]; then
+    echo "initializing database..."
+    sqlite3 $db -init data/files.sql "SELECT * from variantfile"
+    echo "...done"
+fi
+
+# if the refseq table isn't filled in the database already, make it:
+numgenes=$(sqlite3 -bail $db "select * from ncbiRefSeq limit 1;" | wc -l)
+if [[ $numgenes -eq 0 ]]; then
+    echo "adding data to ncbiRefSeq..."
+    awk '{ print "INSERT OR IGNORE INTO ncbiRefSeq (reference_genome, transcript_name, contig, start, end, gene_name) VALUES (" "\047hg37\047, \047" $1 "\047, \047" $2 "\047, " $3 ", " $4 ", \047" $5 "\047);"}' data/refseq/ncbiRefSeqSelect.hg37.txt >> genes.sql
+    awk '{ print "INSERT OR IGNORE INTO ncbiRefSeq (reference_genome, transcript_name, contig, start, end, gene_name) VALUES (" "\047hg38\047, \047" $1 "\047, \047" $2 "\047, " $3 ", " $4 ", \047" $5 "\047);"}' data/refseq/ncbiRefSeqSelect.hg38.txt >> genes.sql
+
+    cat genes.sql | sqlite3 $db
+    rm genes.sql
+    echo "...done"
+fi
--- entrypoint.sh
+++ entrypoint.sh
@@ -28,27 +28,7 @@ if [[ -f "initial_setup" ]]; then
     echo "0 * * * * bash /app/htsget_server/renew_token.sh" >> cron_bkp
     crontab cron_bkp
     rm cron_bkp
-    db=${DB_PATH:-/app/htsget_server/data/files.db}
-
-    # initialize the db if it's not already there:
-    sqlite3 -bail /data/files.db "SELECT * from ncbiRefSeq"
-    if [[ $? -eq 1 ]]; then
-        echo "initializing database..."
-        sqlite3 $db -init /app/htsget_server/data/files.sql "SELECT * from variantfile"
-        echo "...done"
-    fi
-
-    # if the refseq table isn't filled in the database already, make it:
-    numgenes=$(sqlite3 -bail /data/files.db "select * from ncbiRefSeq limit 1;" | wc -l)
-    if [[ $numgenes -eq 0 ]]; then
-        echo "adding data to ncbiRefSeq..."
-        awk '{ print "INSERT OR IGNORE INTO ncbiRefSeq (reference_genome, transcript_name, contig, start, end, gene_name) VALUES (" "\047hg37\047, \047" $1 "\047, \047" $2 "\047, " $3 ", " $4 ", \047" $5 "\047);"}' data/refseq/ncbiRefSeqSelect.hg37.txt >> genes.sql
-        awk '{ print "INSERT OR IGNORE INTO ncbiRefSeq (reference_genome, transcript_name, contig, start, end, gene_name) VALUES (" "\047hg38\047, \047" $1 "\047, \047" $2 "\047, " $3 ", " $4 ", \047" $5 "\047);"}' data/refseq/ncbiRefSeqSelect.hg38.txt >> genes.sql
-
-        cat genes.sql | sqlite3 $db
-        rm genes.sql
-        echo "...done"
-    fi
+    bash create_db.sh
 
     rm initial_setup
 fi
