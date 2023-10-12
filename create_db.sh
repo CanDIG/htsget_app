0a646db96aec01fcf8e48367a285c65abc02d7e6
fnguyen <OrdinaryNeumann@gmail.com>
2023-10-12 12:01:34 -0400

    Fix integration tests for the separated build (but not CI yet)

--- create_db.sh
+++ create_db.sh
@@ -3,11 +3,11 @@
 set -Euo pipefail
 
 # db=${DB_PATH:-data/files.db}
-db="metadata-db"
+db=${DB_PATH:-"metadata-db"}
 
 # PGPASSWORD is used by psql to avoid the password prompt
-export PGPASSWORD=`cat /run/secrets/metadata-db-secret`
-export PGUSER=`cat /run/secrets/metadata-db-user`
+export PGPASSWORD=${PGPASSWORD:-`cat /run/secrets/metadata-db-secret`}
+export PGUSER=${PGUSER:-`cat /run/secrets/metadata-db-user`}
 
 until pg_isready -h metadata-db -p 5432 -U $PGUSER; do
   echo "Waiting for the database to be ready..."
@@ -18,22 +18,22 @@ echo "database is at $db"
 pwd
 
 # initialize the db if it's not already there:
-psql --quiet -h $db -U $PGUSER -d genomic -c "SELECT * from ncbiRefSeq limit 1"
+psql --quiet -h "$db" -U $PGUSER -d genomic -c "SELECT * from ncbiRefSeq limit 1"
 if [[ $? -ne 0 ]]; then
     echo "initializing database..."
-    createdb -h $db -U $PGUSER genomic
-    psql --quiet -h $db -U $PGUSER -a -d genomic -f data/files.sql
+    createdb -h "$db" -U $PGUSER genomic
+    psql --quiet -h "$db" -U $PGUSER -a -d genomic -f data/files.sql
     echo "...done"
 fi
 
 # if the refseq table isn't filled in the database already, make it:
-numgenes=$(psql --quiet -h $db -U $PGUSER -d genomic -c "select * from ncbirefseq where gene_name != '' limit 1;" | wc -l)
+numgenes=$(psql --quiet -h "$db" -U $PGUSER -d genomic -c "select * from ncbirefseq where gene_name != '' limit 1;" | wc -l)
 if [[ $numgenes -lt 5 ]]; then
     echo "adding data to ncbirefseq..."
     awk '{ print "INSERT INTO ncbirefseq (reference_genome, transcript_name, contig, start, endpos, gene_name) VALUES (" "\047hg37\047, \047" $1 "\047, \047" $2 "\047, " $3 ", " $4 ", \047" $5 "\047) ON CONFLICT DO NOTHING;"}' data/refseq/ncbiRefSeqSelect.hg37.txt >> genes.sql
     awk '{ print "INSERT INTO ncbirefseq (reference_genome, transcript_name, contig, start, endpos, gene_name) VALUES (" "\047hg38\047, \047" $1 "\047, \047" $2 "\047, " $3 ", " $4 ", \047" $5 "\047) ON CONFLICT DO NOTHING;"}' data/refseq/ncbiRefSeqSelect.hg38.txt >> genes.sql
 
-    psql --quiet -h $db -U $PGUSER -d genomic -a -f genes.sql
+    psql --quiet -h "$db" -U $PGUSER -d genomic -a -f genes.sql
     # rm genes.sql
     echo "...done"
 fi
