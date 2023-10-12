eda4862e7649f45a674cdef8484bc83a9bd9f4ed
fnguyen <OrdinaryNeumann@gmail.com>
2023-10-12 14:04:06 -0400

    Allow an alternative to secrets when reading passwords

--- htsget_server/config.py
+++ htsget_server/config.py
@@ -9,13 +9,15 @@ config.read(os.path.abspath(f"{os.path.dirname(os.path.realpath(__file__))}/../c
 AUTHZ = config['authz']
 HTSGET_URL = os.getenv("HTSGET_URL", f"http://localhost:{config['DEFAULT']['Port']}")
 
-# DB_PATH = config['paths']['DBPath']
-# if os.environ.get("DB_PATH") is not None:
-#     DB_PATH = f"sqlite:///{os.environ.get('DB_PATH')}"
+if os.environ.get("PGPASSWORD") is not None:
+    password = os.environ.get("PGPASSWORD")
+elif os.environ.get("POSTGRES_PASSWORD_FILE") is not None and os.path.exists(os.environ.get("POSTGRES_PASSWORD_FILE")):
+    with open(os.environ.get("POSTGRES_PASSWORD_FILE"), 'r') as file:
+        password = file.read()
+else:
+    raise Exception("Could not determine how to get PostGres password")
 
-with open(os.environ.get("POSTGRES_PASSWORD_FILE"), 'r') as file:
-    password = file.read()
-    DB_PATH = re.sub("PASSWORD", password, config['paths']['PGPath'])
+DB_PATH = re.sub("PASSWORD", password, config['paths']['PGPath'])
 print(f"Password is: {password}")
 
 CHUNK_SIZE = int(config['DEFAULT']['ChunkSize'])
