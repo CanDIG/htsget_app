1826f61388d0ff5e5c20a9c0037bc106dd8ebda8
fnguyen <OrdinaryNeumann@gmail.com>
2023-10-12 13:55:08 -0400

    Reduce STDOUT output so the TravisCI doesn't fail

--- create_db.sh
+++ create_db.sh
@@ -22,7 +22,7 @@ psql --quiet -h "$db" -U $PGUSER -d genomic -c "SELECT * from ncbiRefSeq limit 1
 if [[ $? -ne 0 ]]; then
     echo "initializing database..."
     createdb -h "$db" -U $PGUSER genomic
-    psql --quiet -h "$db" -U $PGUSER -a -d genomic -f data/files.sql
+    psql --quiet -h "$db" -U $PGUSER -a -d genomic -f data/files.sql >>setup_out.txt
     echo "...done"
 fi
 
@@ -33,7 +33,7 @@ if [[ $numgenes -lt 5 ]]; then
     awk '{ print "INSERT INTO ncbirefseq (reference_genome, transcript_name, contig, start, endpos, gene_name) VALUES (" "\047hg37\047, \047" $1 "\047, \047" $2 "\047, " $3 ", " $4 ", \047" $5 "\047) ON CONFLICT DO NOTHING;"}' data/refseq/ncbiRefSeqSelect.hg37.txt >> genes.sql
     awk '{ print "INSERT INTO ncbirefseq (reference_genome, transcript_name, contig, start, endpos, gene_name) VALUES (" "\047hg38\047, \047" $1 "\047, \047" $2 "\047, " $3 ", " $4 ", \047" $5 "\047) ON CONFLICT DO NOTHING;"}' data/refseq/ncbiRefSeqSelect.hg38.txt >> genes.sql
 
-    psql --quiet -h "$db" -U $PGUSER -d genomic -a -f genes.sql
+    psql --quiet -h "$db" -U $PGUSER -d genomic -a -f genes.sql >>setup_out.txt
     # rm genes.sql
     echo "...done"
 fi
