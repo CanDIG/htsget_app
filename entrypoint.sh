689213233d6a2eb47ff1d549cfe7f36960644ced
Daisie Huang <daisieh@gmail.com>
2024-01-05 21:17:29 -0800

    remove cron stuff here; it's handled by vault-runner

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,18 +22,10 @@ if [[ -f "initial_setup" ]]; then
     sed -i s@\<OPA_URL\>@$OPA_URL@ config.ini
     sed -i s@\<VAULT_URL\>@$VAULT_URL@ config.ini
 
-    # set up crontab
-    sed -i s@\<VAULT_S3_TOKEN\>@$VAULT_S3_TOKEN@ renew_token.sh
-    crontab -l > cron_bkp
-    echo "0 * * * * bash /app/htsget_server/renew_token.sh" >> cron_bkp
-    crontab cron_bkp
-    rm cron_bkp
     bash create_db.sh
 
     rm initial_setup
 fi
-
-cron
 
 # use the following for development
 #python3 htsget_server/server.py
