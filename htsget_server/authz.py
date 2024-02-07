0ee8899a9a6e92e934edcd2a2466bd444c74ea36
Daisie Huang <daisieh@gmail.com>
2024-02-07 11:55:11 -0800

    don't pass in request

--- htsget_server/authz.py
+++ htsget_server/authz.py
@@ -62,8 +62,8 @@ def is_site_admin(request):
     return False
 
 
-def get_s3_url(request, s3_endpoint=None, bucket=None, object_id=None, access_key=None, secret_key=None, region=None, public=False):
-    return authx.auth.get_s3_url(request, s3_endpoint=s3_endpoint, bucket=bucket, object_id=object_id, access_key=access_key, secret_key=secret_key, region=region, public=public)
+def get_s3_url(s3_endpoint=None, bucket=None, object_id=None, access_key=None, secret_key=None, region=None, public=False):
+    return authx.auth.get_s3_url(s3_endpoint=s3_endpoint, bucket=bucket, object_id=object_id, access_key=access_key, secret_key=secret_key, region=region, public=public)
 
 
 def request_is_from_query(request):
