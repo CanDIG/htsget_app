0ffa9bc8d4c617567d0b55c2ba35df1edb9c49ee
Daisie Huang <daisieh@gmail.com>
2024-05-10 21:40:27 -0700

    metadata-db-user isn't a secret

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,6 +23,7 @@ if [[ -f "initial_setup" ]]; then
     sed -i s@\<OPA_URL\>@$OPA_URL@ config.ini
     sed -i s@\<VAULT_URL\>@$VAULT_URL@ config.ini
     sed -i s@\<AGGREGATE_COUNT_THRESHOLD\>@$AGGREGATE_COUNT_THRESHOLD@ config.ini
+    sed -i s@\<POSTGRES_USERNAME\>@$POSTGRES_USERNAME@ config.ini
 
     bash create_db.sh
     mkdir $INDEXING_PATH
