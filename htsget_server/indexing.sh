1c3ccf7dfb69b442ae0c7a849d567170b9ebe459
Daisie Huang <daisieh@gmail.com>
2024-10-30 15:09:41 -0700

    doesn't hurt to message every 10 sec

--- htsget_server/indexing.sh
+++ htsget_server/indexing.sh
@@ -1,6 +1,8 @@
 until python htsget_server/indexing.py; do
     if [[ $? != 10 ]]; then
         echo "Indexing crashed with exit code $?.  Respawning..." >&2
+    else
+        echo "indexer OFF"
     fi
     sleep 10
 done
